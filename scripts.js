dff04cd35345d627364a7f95915c1cce3938cc44
two <twoartwork@gmail.com>
2022-02-06 17:58:04 -0600

    fix some styles and mouse listerners

--- scripts.js
+++ scripts.js
@@ -3,47 +3,44 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const closeButtons = document.querySelectorAll(".close-box");
   let topPos = 2;
 
-  reviews.forEach((item, i) => {
-    if (!item.classList.contains('bio') && !item.classList.contains('boosting')) {
+  reviews.forEach((review) => {
+    if (!review.classList.contains('bio') && !review.classList.contains('boosting')) {
       var left = (Math.random() * (window.innerWidth - 540)).toFixed();
-      var top = (Math.random() * (window.innerHeight - item.offsetHeight - 80)).toFixed();
+      var top = (Math.random() * (window.innerHeight - review.offsetHeight - 80)).toFixed();
 
-      item.style.left = left + 'px';
-      item.style.top = top + 'px'; 
+      review.style.left = left + 'px';
+      review.style.top = top + 'px';
     }
 
-    item.addEventListener('mousedown', function(e) {
-      item.setAttribute('js-moving', '');
+    review.addEventListener('mousedown', function(e) {
+      review.setAttribute('js-moving', '');
       offset = [
-        item.offsetLeft - e.clientX,
-        item.offsetTop - e.clientY
+        review.offsetLeft - e.clientX,
+        review.offsetTop - e.clientY
       ];
+
       topPos = topPos + 1;
-      item.style.zIndex = topPos;
+      review.style.zIndex = topPos;
     }, true);
 
-    item.addEventListener('mouseup', function() {
-      reviews.forEach((other, i) => {
-        other.removeAttribute('js-moving');
-      });
-    }, true);
-
-    document.addEventListener('mouseup', function() {
-      item.removeAttribute('js-moving');
-    }, true);
-
-    item.addEventListener('mousemove', function(event) {
+    review.addEventListener('mousemove', function(event) {
       event.preventDefault();
-      if (item.hasAttribute('js-moving')) {
+      if (review.hasAttribute('js-moving')) {
         mousePosition = {
           x : event.clientX,
-          y : event.clientY
+          y : event.clientY,
         };
-        item.style.left = (mousePosition.x + offset[0]) + 'px';
-        item.style.top  = (mousePosition.y + offset[1]) + 'px';
+        review.style.left = (mousePosition.x + offset[0]) + 'px';
+        review.style.top  = (mousePosition.y + offset[1]) + 'px';
       }
     }, true);
   });
+
+  document.addEventListener('mouseup', function() {
+    reviews.forEach((review) => {
+      review.removeAttribute('js-moving');
+    });
+  }, true);
 
   closeButtons.forEach(closeButton => {
     closeButton.onclick = () => {
